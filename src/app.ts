aaa2e3cb33abf71d8db46f8cb8283bc39bf15dc5
Danilo Da Cruz <danilo.s_95@outlook.com>
2024-12-04 09:02:39 -0300

    Middleware de tratamento de exceções

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
  import express from "express";
- 
- const app = express();
+ import "express-async-errors"
 
- app.use(express.json());
+ import { errorHandling } from "./middlewares/error-handling";
+ 
+ const app = express()
+
+ app.use(express.json())
+
+ app.use(errorHandling)
 
  export { app };
