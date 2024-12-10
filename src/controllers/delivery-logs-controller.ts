3d9ee242deae3fe93735985562ebcf167106a92b
Danilo Da Cruz <danilo.s_95@outlook.com>
2024-12-10 10:19:01 -0300

    Registrando no log alteração do status

--- src/controllers/delivery-logs-controller.ts
+++ src/controllers/delivery-logs-controller.ts
@@ -20,6 +20,10 @@ class DeliveryLogsController {
       throw new AppError("Delivery not found", 404)
     }
 
+    if(delivery.status === "delivered"){
+      throw new AppError("Delivery already delivered", 404)
+    }
+
     if(delivery.status === "processing"){
       throw new AppError("change status to shipped", 404)
     }
@@ -31,6 +35,28 @@ class DeliveryLogsController {
       }
     })
     return response.status(201).json()
+  }
+
+  async show(request: Request, response: Response){
+    const paramsSchema = z.object({
+      delivery_id: z.string().uuid(),
+    })
+
+    const { delivery_id } = paramsSchema.parse(request.params)
+
+    const delivery = await prisma.delivery.findUnique({
+      where: { id: delivery_id },
+      include: {
+        user: true,
+        logs: true
+      }
+    })
+
+    if(request.user?.role === "customer" && request.user.id !== delivery?.userId){
+      throw new AppError("The user can only view their deliveries ", 401)
+    }
+
+    return response.json(delivery)
   }
 }
 
